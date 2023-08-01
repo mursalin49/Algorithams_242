b8857dd7c68b368151f8c28dcea922d1f0adf796
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-08-01 18:44:06 +0600

    Add files via upload

---
+++ lab04/quick_sort_random.cpp
@@ -0,0 +1,60 @@
+
+#include <iostream>
+#include <cstdlib> // For rand() and srand()
+#include <ctime>   // For time()
+
+using namespace std;
+
+int partition(int arr[], int l, int r)
+{
+    // Choosing a random pivot element
+    int randomIndex = l + rand() % (r - l + 1);
+    swap(arr[randomIndex], arr[r]);
+
+    int pivot = arr[r];
+    int i = l - 1;
+
+    for (int j = l; j < r; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    swap(arr[i + 1], arr[r]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int p = partition(arr, l, r);
+        quickSort(arr, l, p - 1);
+        quickSort(arr, p + 1, r);
+    }
+}
+
+int main()
+{
+    srand(time(0)); // Seed the random number generator with the current time
+
+    int n;
+    cout << "Enter an array size: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    quickSort(arr, 0, n - 1);
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
